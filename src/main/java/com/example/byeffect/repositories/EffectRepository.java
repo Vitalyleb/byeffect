6a682fbff1f0e7d9b446bce4e49b92af0c900853
Vitalyleb <lebedevvn2001@mail.ru>
2023-03-10 19:22:03 +0700

    Initial commit

--- src/main/java/com/example/byeffect/repositories/EffectRepository.java
+++ src/main/java/com/example/byeffect/repositories/EffectRepository.java
@@ -3,8 +3,6 @@ package com.example.byeffect.repositories;
 import com.example.byeffect.models.Effect;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface EffectRepository extends JpaRepository<Effect, Long> {
-    List<Effect> findByTitle(String title);
+
 }
