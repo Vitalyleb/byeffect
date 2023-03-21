188b952bd121e16dff889e87baa85d5114c043e0
Vitalyleb <71664893+Vitalyleb@users.noreply.github.com>
2023-03-21 15:37:40 +0700

    Update EffectRepository.java

--- src/main/java/com/example/byeffect/repositories/EffectRepository.java
+++ src/main/java/com/example/byeffect/repositories/EffectRepository.java
@@ -4,5 +4,6 @@ import com.example.byeffect.models.Effect;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EffectRepository extends JpaRepository<Effect, Long> {
+  List<Effect> findByTitle(String title);
 
 }
